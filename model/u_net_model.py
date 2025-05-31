866d097db58e7fea4a673dae4c023a771c91aa8c
Fatih Jawwad <wedjaw22@gmail.com>
2025-05-31 06:58:27 +0000

    make full U-Net full model

---
+++ model/u_net_model.py
@@ -0,0 +1,10 @@
+from .u_net_part import *
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = (Do)
